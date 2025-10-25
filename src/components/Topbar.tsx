3b8ea59f2262fa314a201ee51ce7f49bf9f93f52
nersaview45abusiness-cmd <nersaview45abusiness@gmail.com>
2025-10-25 22:25:04 +0700

    Updated Topbar.tsx

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -43,8 +43,8 @@ export const Topbar = (): JSX.Element => {
       </div>
 
       <div className="flex items-center gap-3">
-        <img className="w-auto h-[42px]" alt="Button group" src="/button-group.svg" />
-        <Avatar className="w-[42px] h-[42px]">
+        <img className="w-auto h-[30px]" alt="Button group" src="/button-group.svg" />
+        <Avatar className="w-[30px] h-[30px]">
           <AvatarImage src="/ellipse-2082.png" alt="User avatar" />
           <AvatarFallback>U</AvatarFallback>
         </Avatar>
